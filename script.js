d5951069b9ca11f07055fab99c6be7068ba94341
Skyler Basco <58148243+skylerbasco@users.noreply.github.com>
2021-10-28 19:40:51 -0400

    fixing number counter animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-
+/*
 const counters = document.querySelectorAll('.counter');
-const speed = 50; // The lower the slower
+const speed = 200; // The lower the slower
 
 counters.forEach(counter => {
 	const updateCount = () => {
@@ -28,4 +28,5 @@ counters.forEach(counter => {
 	
 });
 
+*/
 
